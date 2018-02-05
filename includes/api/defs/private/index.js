02659306720611c4d24d9ca2af5a3456be8b53b7
Nicholas Rios <nicholas.jay.rios@gmail.com>
2018-02-05 05:45:15 -0600

    updated

--- includes/api/defs/private/index.js
+++ includes/api/defs/private/index.js
@@ -1,17 +1,34 @@
 module.exports = {
-	googleKey:'',
-	googleCX:'',
-	indeedID:'',
-	zillowID:'',
-	youtubeKey:'',
-	instagramToken:'',
-	sunlightKey:'',
-	meetupKey:'',
-	googleCivicInfoKey:{apiKey: ''},
-	yelpBusiness:{},
-	twitterAuth:[],
-	foursquareConfig:{},
-	flickrAuth:{},
-	locuKeys:{}
+	googleKey:'AIzaSyDB4jJNBnXVgD5M-PRDNicjzyv7famrhCw',
+	googleCX:'003442673057133302759:b1syggqpi8m',
+	indeedID:'5402905905748372',
+	zillowID:'X1-ZWz19kz12e2z9n_2doog',
+	youtubeKey:'AIzaSyDB4jJNBnXVgD5M-PRDNicjzyv7famrhCw',
+	instagramToken:'206421603.06b5b88.c2dc8017c6ff4a8d88a41cd1a338e580',
+	sunlightKey:'e53c23d202644e569a84b09d73d418ef',
+	meetupKey:'63b5362815241e361d58533d1d662',
+	googleCivicInfoKey:{apiKey: 'AIzaSyDbQ2Qk-k1hdsbgAQfgNgpndl7lxujLBQg'},
+	yelpBusiness:{
+			  consumer_key: 'h5a-LThMlFTJivtXNJIjQQ',
+			  consumer_secret: 'GOza2lCRzq0MBaDOwOE0FYDJW9s',
+			  token: 'HxhedpuftOB7yEQsZyW3egxlCuz7lqSb',
+			  token_secret: 'hcWKmwdKr29HWA2u8s4FQxWxHgo',
+	},
+	twitterAuth:['G8kLIo4QdNkA8eBsGLI5kp4EB',
+				   'Ya4mmkTxNd3EvULVl1jx13ogND0PaHoOlKPd7wMW6Dwi0a9y27',
+				   '1826366814-sEHLt7fWaPDQGMuldQTypZsO78lnBvRPIMG0SFn',
+				   'UD7OntjLwBCqsqdKzuBmVnHW1iFSSISJnOjEeqVKoQrGz'],
+	foursquareConfig:{
+		id:'C2ZFFIJ0JVQNQN5YMFKWBHRTAEN01SPWZWEETPAB3HZZBZVG',
+		secret:'Q0HQBCOLG4V0JNPTLQEYCZ4L0MGB5NPXZRS0CG2Q4WRP1MUW'
+	},
+	flickrAuth:{
+		key:'4098e2a2fc5c2cf31200b1b0601c3aae',
+		secret:'5479dc644c57759f'
+	},
+	locuKeys:{
+		api:'063a235be34fc56794762ff37a9fca9c461b71a5',
+		widget:'8b63090b64d2cc607a2f92df35b72eb9df6b5dbf'
+	}
 }
 
